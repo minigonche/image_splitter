ce00b23b2e6db2116b4372cdbbf15917c23af2ba
minigonche <minigonche@gmail.com>
2017-02-13 11:52:16 +0000

    Initial changes

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# image_splitter
+# PDF Splitter
+
+This is a simply Python script that processes a PDF and plits each page vertically in two.
+It is ment for scanned books.
+
+Simply run the script with the location of the file and the script does the rest.
--- script.py
+++ script.py
@@ -1,16 +1,44 @@
-from PIL import Image
-import Image
+#Script for dividing pdf in two
+from pyPdf import PdfFileWriter, PdfFileReader
+import copy, sys
 
-def crop(Path, input, height, width, k, page, area):
-    im = Image.open(input)
-    imgwidth, imgheight = im.size
-    for i in range(0,imgheight,height):
-        for j in range(0,imgwidth,width):
-            box = (j, i, j+width, i+height)
-            a = im.crop(box)
-            try:
-                o = a.crop(area)
-                o.save(os.path.join(Path,"PNG","%s" % page,"IMG-%s.png" % k))
-            except:
-                pass
-            k +=1
+
+def split(file_name):
+
+    input1 = PdfFileReader(file(file_name, "rb"))
+    output = PdfFileWriter()
+    
+    numPages = input1.getNumPages()
+    print "document has %s pages." % numPages
+    
+    for i in range(numPages):
+        page1 = input1.getPage(i)
+        page2 = copy.copy(page1)
+        w = page1.mediaBox.getUpperRight_y()
+        h = page1.mediaBox.getUpperRight_x()
+        #The width and height are weird
+        page1.cropBox.lowerLeft = (0, 0)
+        page1.cropBox.upperRight = (h, w/2)
+        
+        page2.cropBox.lowerLeft = ( 0,w/2)
+        page2.cropBox.upperRight = (h, w)
+        
+        output.addPage(page1)
+        output.addPage(page2)
+        
+        
+        
+    
+    
+    outputStream = file("out.pdf", "wb")
+    output.write(outputStream)
+    outputStream.close()    
+    
+    print 'Finished'   
+    
+
+if __name__ == "__main__":
+    
+    split(sys.argv[1])            
+            
+            
